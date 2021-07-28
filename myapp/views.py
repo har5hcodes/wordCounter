660fcdfe07034efdcaf0b45b3947c615a5788fce
harshwandr <harshavardhanputta5@gmail.com>
2021-07-28 18:36:57 +0530

    django project

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def counter(request):
+    words = request.POST['words']
+    no_of_words = len(words.split())
+    return render(request, 'counter.html', {'number': no_of_words})
